9308666c93d8403903d5d70932a339871eb390cd
Kevin Loughead <kvnloughead@gmail.com>
2020-01-11 16:01:41 -0500

    Added basic guessing logic.  See commit-comments.md

--- script.js
+++ script.js
@@ -8,7 +8,26 @@ function $All(elem){
 var colors = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 255, 255)",
               "rgb(0, 0, 255)", "rgb(255, 0, 255)"]
 
-var squares = $All('.squares')
+var squares = $All('.squares');
+var colorToGuess = colors[3];
+var colorToGuessDisplay = $('h1 span');
+var messageDisplay = $('#message');
+
 for (var i = 0; i < squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
+    squares[i].addEventListener('click', function(){
+        // get color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === colorToGuess){
+            for (const square of squares){
+                square.style.backgroundColor = colorToGuess;
+            }
+            messageDisplay.textContent = 'Correct!';
+        } else {
+            this.style.backgroundColor = '#232323';
+            messageDisplay.textContent = 'Try Again'
+        }
+    })
 }
+
+colorToGuessDisplay.textContent = colorToGuess;
